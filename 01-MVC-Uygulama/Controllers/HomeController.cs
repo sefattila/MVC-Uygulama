3f5ef0aabf1370fdd6fb4fcd99882b4140856629
sefattila <rq7fans@gmail.com>
2023-09-20 12:06:14 +0300

    Add project files.

---
+++ 01-MVC-Uygulama/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using _01_MVC_Uygulama.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _01_MVC_Uygulama.Controllers
+{
+    public class HomeController : Controller
+    {
+        //action 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //action
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Attila()
+        {
+            return View();
+        }
+    }
+}
